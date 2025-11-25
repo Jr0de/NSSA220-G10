1d6d8bd95df285345d0d436a1175915acd6c96ba
jabber-jay <jbs1660@g.rit.edu>
2025-11-24 19:50:44 -0500

    Phase 1

---
+++ filter_packets.py
@@ -0,0 +1,12 @@
+#Phase 1
+
+def filter() :
+    #Create loop to read the PCAP and text files for each node
+
+    #Read/split each line as needed
+
+    #Only process ICMP Echo Request and ICMP Echo Reply packets 
+
+    #Output new filtered file (Node*_filtered.txt)
+
+    print()
