eaa826922d469a5ad6c31b98286bd95deb7a5772
jabber-jay <jbs1660@g.rit.edu>
2025-11-24 19:51:28 -0500

    Phase 3

---
+++ compute_metrics.py
@@ -0,0 +1,40 @@
+#Phase 3
+
+def compute() :
+    #Create loop to do the following Data size metrics:
+    
+    #1. Number of Echo Requests sent
+
+    #2. Number of Echo Requests received
+
+    #3. Number of Echo Replies sent
+
+    #4. Number of Echo Replies received
+
+    #5. Total Echo Request bytes sent: In bytes, based on the size of the “frame”
+
+    #6. Total Echo Request bytes received: In bytes, based on the size of the “frame”
+
+    #7. Total Echo Request data sent: In bytes, based on amount of data in the ICMP payload
+
+    #8. Total Echo Request data received: In bytes, based on amount of data in the ICMP payload
+
+
+    #Create loop to do the following Time based metrics:
+
+    #1. Average Ping Round Trip Time (RTT): Measured in milliseconds
+
+    #2. Echo Request Throughput (in kB/sec): 
+
+    #3. Echo Request Goodput (in kB/sec):
+
+    #4. Average Reply Delay (in microseconds): 
+
+
+    #Create loop to do the following Distance metric:
+
+    #Average number of hops per Echo Request, cannot hard code
+    print()
+
+
+#Output
