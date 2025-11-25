845960d7eb46e1ce4dafb3bfc2c772c01897b371
jabber-jay <jbs1660@g.rit.edu>
2025-11-24 19:51:10 -0500

    Phase 2

---
+++ packet_parser.py
@@ -0,0 +1,13 @@
+#Phase 2
+
+#Input file
+
+def parse() :
+    #Seperate files by lines, split by commas if needed
+
+    #Parse the filtered raw text files and read packet fields
+
+    #Choose to parse the summary line text or the hex
+
+    #Only output the data needed for the metrics
+    print()
