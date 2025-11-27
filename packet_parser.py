e9212b4bd7131a8c17f318c492e592b76f6855e5
Jr0de <dotsandbitts@gmail.com>
2025-11-27 03:30:52 -0500

    hex_to_ip function complete and added folder for provided files

--- packet_parser.py
+++ packet_parser.py
@@ -2,6 +2,15 @@
 
 #Input file
 
+# Given a list of 4 2-character hex strings, return an ipv4 address string
+def hex_to_ip(hexlist: list[str]) -> str:
+    ip = ""
+    for byte_hex in hexlist:
+        byte_int = int(byte_hex, 16)# Convert hex string to decimal int, specifying base 16 (hexidecimal)
+        ip += str(byte_int) + '.' # Append the decimal byte to the full ip string, along with a '.' to denote separation
+
+    return ip[0:-1]# Return the ip string without the last character, as it is a uselsss '.'
+
 def parse() :
     #Seperate files by lines, split by commas if needed
 
@@ -11,3 +20,9 @@ def parse() :
 
     #Only output the data needed for the metrics
     print()
+
+def main():
+    print(hex_to_ip(['c0', 'a8', '64', '01']))
+
+if __name__ == '__main__':
+    main()
